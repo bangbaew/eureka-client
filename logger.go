ddcbfb10e68f6623b37128d5057285cc2d6b51c5
Jatutep Bunupuradah <bangbaew@Gigabyte-B460M-DS3H.local>
2023-03-25 15:19:02 +0700

    better logging
    Add env: LOG_LEVEL=ERROR / WARN / INFO / DEBUG, if not set, default to INFO

--- logger.go
+++ logger.go
@@ -1,6 +1,38 @@
 package eureka_client
 
-import "log"
+import (
+	"log"
+	"os"
+)
+
+const (
+	DEBUG = 4
+	INFO  = 3
+	WARN  = 2
+	ERROR = 1
+)
+
+const (
+	Reset  = "\033[0m"
+	Red    = "\033[31m"
+	Green  = "\033[32m"
+	Yellow = "\033[33m"
+	Cyan   = "\033[36m"
+)
+
+var logLevel = func() int {
+	switch os.Getenv("LOG_LEVEL") {
+	case "DEBUG":
+		return DEBUG
+	case "INFO":
+		return INFO
+	case "WARN":
+		return WARN
+	case "ERROR":
+		return ERROR
+	}
+	return INFO
+}()
 
 type Logger interface {
 	Debug(msg string)
@@ -13,19 +45,27 @@ type DefaultLogger struct {
 }
 
 func (*DefaultLogger) Debug(msg string) {
-	log.Println(msg)
+	if logLevel >= DEBUG {
+		log.Println(Green + "DEBUG: " + Reset + msg)
+	}
 }
 
 func (*DefaultLogger) Info(msg string) {
-	log.Println(msg)
+	if logLevel >= INFO {
+		log.Println(Cyan + "INFO: " + Reset + msg)
+	}
 }
 
 func (*DefaultLogger) Warn(msg string, err error) {
-	log.Printf("%s, error: %v", msg, err)
+	if logLevel >= WARN {
+		log.Printf("%sWARNING: %s%s, %v\n", Yellow, Reset, msg, err)
+	}
 }
 
 func (*DefaultLogger) Error(msg string, err error) {
-	log.Printf("%s, error: %v", msg, err)
+	if logLevel >= ERROR {
+		log.Printf("%sERROR: %s%s, %v\n", Red, Reset, msg, err)
+	}
 }
 
 func NewLogger() Logger {
